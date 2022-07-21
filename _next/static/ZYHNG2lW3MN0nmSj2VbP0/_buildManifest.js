acddebf2ffa3f2782a495f9dd874f294b94535bc
nik19ta <44050303+nik19ta@users.noreply.github.com>
2022-07-21 13:42:10 +0000

    Deploying to gh-pages from @ nik19ta/nik19ta.me@78240d1fe79a1625ef03f677967eefca56543cdc 🚀

--- _next/static/ZYHNG2lW3MN0nmSj2VbP0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/517-7386701efb2a8437.js","static/css/40f4ca576ad9bb22.css","static/chunks/pages/index-c009b1de19c67297.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
