5056ad45d6a9a50b155bdd6ce61325559ff95b00
Nikita Khvatov <nikhvatov19@gmail.com>
2022-07-25 13:56:31 +0300

    Updates

--- _next/static/yZrsy3xPgkIqzWov2teql/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/517-7386701efb2a8437.js","static/css/0f7d5591e527e697.css","static/chunks/pages/index-bb7d547e2ca7f674.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
